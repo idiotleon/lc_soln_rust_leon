dbfa7903e56f54d2ce0d3fce1c839c979195a540
Leon <yanglyu.leon.7@gmail.com>
2020-12-09 17:30:08 -0800

    start: Leetcode Solutions in Rust

---
+++ src/leetcode/ood/lvl3/lc0173/soln_0_stack.rs
@@ -0,0 +1,26 @@
+
+
+struct BSTIterator{
+    stack: Vec<TreeNode>
+}
+
+impl BSTIterator{
+
+    fn new(root: Option<Rc<RefCell<TreeNode>>>) -> Self {
+        BSTIterator{
+            stack = Vec:new()
+        }
+    }
+
+    fn next(&self) -> i32{
+
+    }
+
+    fn has_next(&self) -> bool{
+
+    }
+
+    fn push_left(&self, node: TreeNode){
+        while node != 
+    }
+}
