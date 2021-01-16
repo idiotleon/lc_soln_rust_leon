1f4491a4e5c627bcb021d3de2c44428bf1d219e6
Leon <yanglyu.leon.7@gmail.com>
2021-01-15 20:28:29 -0800

    reviews

---
+++ src/leetcode/lvl2/lc0001/soln_0_hashmap.rs
@@ -0,0 +1,23 @@
+/// https://leetcode.com/problems/two-sum/
+/// 
+/// Time Complexity:    O()
+/// Space Complexity:   O()
+/// 
+/// References:
+///     https://leetcode.com/problems/two-sum/discuss/715951/Rust%3A-HashMap-solution
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut num_to_idx: HashMap<i32, i32> = HashMap::new();
+        
+        for (idx, num) in nums.iter().enumerate(){
+            match num_to_idx.get(&(target - *num)){
+                Some(&idx2) => return vec![idx as i32, idx2],
+                None => num_to_idx.insert(*num, idx as i32),
+            };
+        }
+        
+        unreachable!()
+    }
+}
---
+++ src/leetcode/lvl2/lc0001/soln_1_brtual_force.rs
@@ -0,0 +1,21 @@
+/// https://leetcode.com/problems/two-sum/
+/// 
+/// Time Complexity:    O()
+/// Space Complexity:   O()
+///
+/// References:
+///     https://leetcode.com/problems/two-sum/discuss/207679/Rust
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        for lo in 0..nums.len(){
+            for hi in lo + 1..nums.len(){
+                if nums[lo] + nums[hi] == target {
+                    return vec![lo as i32, hi as i32]
+                }
+            }
+        }
+        
+        unreachable!()
+    }
+}
