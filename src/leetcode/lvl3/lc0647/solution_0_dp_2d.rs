d366bae5fc233d3d52f782f306a3677d496e2863
Leon <yanglyu.leon.7@gmail.com>
2021-03-17 22:06:33 -0700

    review

---
+++ src/leetcode/lvl3/lc0647/solution_0_dp_2d.rs
@@ -0,0 +1,33 @@
+// https://leetcode.com/problems/palindromic-substrings/
+//
+// Time Complexity:     O()
+// Space Complexity:    O()
+//
+// Reference:
+//  https://leetcode.com/problems/palindromic-substrings/discuss/258917/Java-Simple-Code%3A-DP-short
+impl Solution {
+    pub fn count_substrings(str: String) -> i32 {
+        const LEN_S: usize = str.len();
+        let mut dp = [[false; LEN_S]; LEN_S];
+        let mut count: i32 = 0;
+
+        for len in 0..LEN_S {
+            for lo in 0..LEN_S - len {
+                let hi = lo + len;
+
+                if str.chars().nth(lo).unwrap() == str.chars().nth(hi).unwrap() {
+                    dp[lo][hi] = if hi - lo + 1 <= 3 {
+                        true
+                    } else {
+                        dp[lo + 1][hi - 1]
+                    };
+                }
+
+                if dp[lo][hi] {
+                    count += 1;
+                }
+            }
+        }
+        return count;
+    }
+}
