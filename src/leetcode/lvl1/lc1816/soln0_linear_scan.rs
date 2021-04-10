9283e075f8e777b40c1cbe40be262d9403309b7b
Leon <yanglyu.leon.7@gmail.com>
2021-04-09 20:55:26 -0700

    review

--- src/leetcode/lvl1/lc1816/soln0_linear_scan.rs
+++ src/leetcode/lvl1/lc1816/soln0_linear_scan.rs
@@ -1,25 +1,22 @@
 /// @author: Leon
 /// https://leetcode.com/problems/truncate-sentence/
-/// 
+///
 /// Time Complexity:  O(L)
 /// Space Complexity: O(1)
 impl Solution {
     pub fn truncate_sentence(str: String, k: i32) -> String {
         let mut cnt: i32 = 0;
-        
         let mut ans = String::new();
-        
-        for ch in str.chars(){
-            if ch == ' '{
+
+        for ch in str.chars() {
+            if ch == ' ' {
                 cnt += 1;
-                if cnt == k{
+                if cnt == k {
                     break;
                 }
             }
-            
             ans.push(ch);
         }
-        
         ans
     }
 }
