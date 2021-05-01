98c64258c917e62f410ae1b3988cca460bcb80a4
Leon <yanglyu.leon.7@gmail.com>
2021-05-01 12:29:29 -0700

    review

---
+++ src/leetcode/lvl3/lc0323/soln_0_bfs.rs
@@ -0,0 +1,61 @@
+// @author: Leon
+// https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+//
+// Time Complexity:     O(V + E)
+// Space Complexity:    O(V + E)
+use std::collections::HashSet;
+use std::collections::VecDeque;
+
+impl Solution {
+    pub fn count_components(n: i32, edges: Vec<Vec<i32>>) -> i32 {
+        let graph = Self::build_graph(n as usize, &edges);
+        let mut visited: HashSet<i32> = HashSet::with_capacity(n as usize);
+        let mut cnt = 0;
+
+        for i in 0..n {
+            if visited.contains(&i) {
+                continue;
+            }
+
+            Self::bfs(i, &mut visited, &graph);
+            cnt += 1;
+        }
+
+        cnt
+    }
+
+    fn bfs(start: i32, mut visited: &mut HashSet<i32>, graph: &Vec<Vec<i32>>) {
+        let mut queue: VecDeque<i32> = VecDeque::new();
+        queue.push_back(start);
+
+        while !queue.is_empty() {
+            let size = queue.len();
+
+            for sz in 0..size {
+                if let Some(top) = queue.pop_front() {
+                    if !visited.insert(top) {
+                        continue;
+                    }
+
+                    for &next in graph[top as usize].iter() {
+                        queue.push_back(next);
+                    }
+                }
+            }
+        }
+    }
+
+    fn build_graph(n: usize, edges: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+        let mut graph: Vec<Vec<i32>> = vec![Vec::with_capacity(n); n];
+
+        for edge in edges.iter() {
+            let u = edge[0];
+            let v = edge[1];
+
+            graph[u as usize].push(v);
+            graph[v as usize].push(u);
+        }
+
+        graph
+    }
+}
---
+++ src/leetcode/lvl3/lc0323/soln_0_union_find.rs
@@ -0,0 +1,58 @@
+// @author: Leon
+// https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+//
+// Time Complexity:     O(`n`)
+// Space Complexity:    O(`n`)
+impl Solution {
+    pub fn count_components(n: i32, edges: Vec<Vec<i32>>) -> i32 {
+        let n = n as usize;
+        let mut roots = vec![0; n];
+        let mut ranks = vec![0; n];
+        for i in 0..n{
+            roots[i] = i;
+            ranks[i] = 1;
+        }
+
+        let mut count = n;
+        for edge in edges.iter(){
+            let u = edge[0] as usize;
+            let v = edge[1] as usize;
+
+            Self::union(u, v, &mut roots, &mut ranks, &mut count)
+        }
+
+        count as i32
+    }
+
+    fn find(x: usize, mut roots: &mut Vec<usize>) -> usize{
+        let mut x = x;
+        while x != roots[x]{
+            roots[x] = roots[roots[x]];
+            x = roots[x];
+        }
+
+        x
+    }
+
+    fn union(x: usize, y: usize,
+             mut roots: &mut Vec<usize>,
+             mut ranks: &mut Vec<usize>,
+             mut count: &mut usize) {
+        let root_x = Self::find(x, &mut roots);
+        let root_y = Self::find(y, &mut roots);
+
+        if root_x == root_y {
+            return
+        }
+
+        if ranks[root_x] > ranks[root_y]{
+            roots[root_y] = root_x;
+            ranks[root_x] += 1;
+        }else{
+            roots[root_x] = root_y;
+            ranks[root_y] += 1;
+        }
+
+        *count -= 1;
+    }
+}
