bd01c7682ea60492fb9ccfe72c4920cdcebbb691
Leon <yanglyu.leon.7@gmail.com>
2021-05-02 00:40:54 -0700

    review

---
+++ src/leetcode/lvl3/lc0011/soln_0_two_ptrs.rs
@@ -0,0 +1,26 @@
+// @author: Leon 
+// https://leetcode.com/problems/container-with-most-water/
+//
+// Time Complexity:     O(`size`)
+// Space Complexity:    O(1)
+use std::cmp;
+
+impl Solution {
+    pub fn max_area(heights: Vec<i32>) -> i32 {
+        let size = heights.len();
+        
+        let mut water: i32 = 0;
+        let mut lo = 0;
+        let mut hi = size - 1;
+        
+        while(lo < hi){
+            let height = cmp::min(heights[lo], heights[hi]) as i32;
+            water = cmp::max(water, (hi - lo) as i32 * height);
+            
+            while lo < hi && heights[lo] <= height { lo += 1; }
+            while lo < hi && heights[hi] <= height { hi -= 1; }
+        }
+        
+        water
+    }
+}
