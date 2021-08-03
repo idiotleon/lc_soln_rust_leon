7f3601155062992e6300f8335885cdd738b62724
Leon <yanglyu.leon.7@gmail.com>
2021-08-02 18:18:05 -0700

    review

--- src/leetcode/lvl2/lc0001/soln_0_hashmap.rs
+++ src/leetcode/lvl2/lc0001/soln_0_hashmap.rs
@@ -1,10 +1,13 @@
+/// @author: Leon
 /// https://leetcode.com/problems/two-sum/
 /// 
-/// Time Complexity:    O()
-/// Space Complexity:   O()
+/// Time Complexity:    O(`len_n`)
+/// Space Complexity:   O(`len_n`)
+/// 
+/// this version consumes the original vector
 /// 
 /// References:
-///     https://leetcode.com/problems/two-sum/discuss/715951/Rust%3A-HashMap-solution
+/// https://leetcode.com/problems/two-sum/discuss/715951/Rust%3A-HashMap-solution
 use std::collections::HashMap;
 
 #[allow(dead_code)]
@@ -13,13 +16,18 @@ struct Solution;
 #[allow(dead_code)]
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut num_to_idx: HashMap<i32, i32> = HashMap::new();
+        // not used
+        // let len_n = nums.len();
+
+        let mut sum_to_idx = HashMap::<i32, usize>::new();
         
-        for (idx, num) in nums.iter().enumerate(){
-            match num_to_idx.get(&(target - *num)){
-                Some(&idx2) => return vec![idx as i32, idx2],
-                None => num_to_idx.insert(*num, idx as i32),
-            };
+        for (idx, num) in nums.into_iter().enumerate(){
+            let expected_sum = target - num;
+            
+            match sum_to_idx.get(&expected_sum){
+                Some(&prev_idx) => return vec![prev_idx as i32, idx as i32],
+                _ => {sum_to_idx.insert(num, idx);},
+            }
         }
         
         unreachable!()
--- src/leetcode/lvl2/lc0001/soln_1_brutal_force.rs
+++ src/leetcode/lvl2/lc0001/soln_1_brutal_force.rs
@@ -4,16 +4,17 @@
 /// Space Complexity:   O()
 ///
 /// References:
-///     https://leetcode.com/problems/two-sum/discuss/207679/Rust
-/// 
+/// https://leetcode.com/problems/two-sum/discuss/207679/Rust
 #[allow(dead_code)]
 struct Solution;
 
 #[allow(dead_code)]
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        for lo in 0..nums.len(){
-            for hi in lo + 1..nums.len(){
+        let len_n = nums.len();
+
+        for lo in 0..len_n{
+            for hi in lo + 1..len_n{
                 if nums[lo] + nums[hi] == target {
                     return vec![lo as i32, hi as i32]
                 }
