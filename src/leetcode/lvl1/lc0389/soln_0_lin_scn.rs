51047b9b6536743351719093fb883e4537a9ee33
Leon <yanglyu.leon.7@gmail.com>
2022-02-28 00:00:29 -0800

    review

--- src/leetcode/lvl1/lc0389/soln_0_lin_scn.rs
+++ src/leetcode/lvl1/lc0389/soln_0_lin_scn.rs
@@ -9,10 +9,13 @@ impl Solution {
     pub fn find_the_difference(s: String, t: String) -> char {
         let _len_s: usize = s.len();
         let _len_t: usize = t.len();
-        let mut freqs: Vec<i16> = vec![0; 26];
-        for ch in s.chars() {
-            freqs[ch as usize - 'a' as usize] += 1;
-        }
+        let mut freqs: Vec<i16> = {
+            let mut freqs: Vec<i16> = vec![0; 26];
+            for ch in s.chars() {
+                freqs[ch as usize - 'a' as usize] += 1;
+            }
+            freqs
+        };
         for ch in t.chars() {
             let idx = ch as usize - 'a' as usize;
             freqs[idx] -= 1;
