61ac1d6c46eaa193cc1356ea62c7d09f8f51ac9a
Leon <yanglyu.leon.7@gmail.com>
2022-03-06 12:20:12 -0800

    refactor

--- src/leetcode/lvl4/lc0042/soln_0_two_ptrs.rs
+++ src/leetcode/lvl4/lc0042/soln_0_two_ptrs.rs
@@ -1,30 +1,24 @@
+/// @author: Leon
 /// https://leetcode.com/problems/trapping-rain-water/
-///
 /// Time Complexity:    O(`len`)
 /// Space Complexity:   O(1)
-///
 /// Reference:
 /// https://leetcode.com/problems/trapping-rain-water/discuss/17391/Share-my-short-solution./185869
 /// https://leetcode.com/problems/trapping-rain-water/discuss/17391/Share-my-short-solution.
-#[allow(dead_code)]
 struct Solution;
 
 #[allow(dead_code)]
 impl Solution {
     pub fn trap(heights: Vec<i32>) -> i32 {
-        let len: usize = heights.len();
-
+        let len_hts: usize = heights.len();
         let mut lo: i32 = 0;
-        let mut hi: i32 = len as i32 - 1;
+        let mut hi: i32 = len_hts as i32 - 1;
         let mut lo_max: i32 = 0;
         let mut hi_max: i32 = 0;
-
         let mut water: i32 = 0;
-
         while lo <= hi {
             lo_max = std::cmp::max(lo_max, heights[lo as usize]);
             hi_max = std::cmp::max(hi_max, heights[hi as usize]);
-
             if lo_max < hi_max {
                 water += lo_max - heights[lo as usize];
                 lo += 1;
@@ -33,7 +27,6 @@ impl Solution {
                 hi -= 1;
             }
         }
-
         water
     }
 }
