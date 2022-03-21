3e84827b4dca4a236609e01e149f687d351a6a98
Leon <yanglyu.leon.7@gmail.com>
2022-03-21 11:15:20 -0700

    review

--- src/leetcode/lvl3/lc0763/soln_0_lin_scn.rs
+++ src/leetcode/lvl3/lc0763/soln_0_lin_scn.rs
@@ -1,11 +1,9 @@
+/// @author: Leon
 /// https://leetcode.com/problems/partition-labels/
-///
 /// Time Complexity:    O(`_len_s`)
 /// Space Complexity:   O(1)
-///
 /// Reference:
 /// https://leetcode.com/problems/partition-labels/discuss/113259/Java-2-pass-O(n)-time-O(1)-space-extending-end-pointer-solution
-#[allow(dead_code)]
 struct Solution;
 
 #[allow(dead_code)]
@@ -13,15 +11,18 @@ impl Solution {
     pub fn partition_labels(s: String) -> Vec<i32> {
         let _len_s = s.len();
         let chs: Vec<char> = s.chars().collect();
-        let mut last_idxes: Vec<usize> = vec![0; 26];
-        for (idx, &ch) in chs.iter().enumerate() {
-            last_idxes[ch as usize - 'a' as usize] = idx;
-        }
+        let last_indices: Vec<usize> = {
+            let mut last_indices: Vec<usize> = vec![0; 26];
+            for (idx, &ch) in chs.iter().enumerate() {
+                last_indices[ch as usize - 'a' as usize] = idx;
+            }
+            last_indices
+        };
         let mut lo: usize = 0;
         let mut hi: usize = 0;
         let mut ans: Vec<i32> = Vec::new();
-        for (idx, &ch) in chs.iter().enumerate() {
-            hi = std::cmp::max(hi, last_idxes[ch as usize - 'a' as usize]);
+        for (idx, ch) in chs.into_iter().enumerate() {
+            hi = std::cmp::max(hi, last_indices[ch as usize - 'a' as usize]);
             if hi == idx {
                 ans.push((hi - lo + 1) as i32);
                 lo = hi + 1;
