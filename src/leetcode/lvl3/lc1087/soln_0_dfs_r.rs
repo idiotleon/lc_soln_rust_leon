e9f894f3dddd445aed6a732743b85aa720c2df12
Leon <yanglyu.leon.7@gmail.com>
2022-04-12 23:22:43 -0700

    refactor

--- src/leetcode/lvl3/lc1087/soln_0_dfs_r.rs
+++ src/leetcode/lvl3/lc1087/soln_0_dfs_r.rs
@@ -4,6 +4,7 @@
 /// Space Complexity:   O()
 struct Solution;
 
+#[allow(dead_code)]
 impl Solution {
     // pub fn expand(s: String) -> Vec<String> {}
     // fn extract(chs: &Vec<char>) -> Vec<Vec<char>> {
