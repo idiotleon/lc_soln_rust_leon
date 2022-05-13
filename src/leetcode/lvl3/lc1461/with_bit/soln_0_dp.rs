6c3f8ee310a34a0e7c01647bcef863ef244888b5
Leon <yanglyu.leon.7@gmail.com>
2022-05-12 21:35:24 -0700

    review

--- src/leetcode/lvl3/lc1461/with_bit/soln_0_dp.rs
+++ src/leetcode/lvl3/lc1461/with_bit/soln_0_dp.rs
@@ -1,6 +1,6 @@
 /// @author: Leon
 /// https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
-/// Time Complexity:    O(`len_s`)
+/// Time Complexity:    O(`_len_s`)
 /// Space Complexity:   O(2 ^ `k`)
 /// Reference:
 /// https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/discuss/1105774/Rust-0ms-one-line-64ms-solution
@@ -9,23 +9,23 @@ struct Solution;
 #[allow(dead_code)]
 impl Solution {
     pub fn has_all_codes(s: String, k: i32) -> bool {
-        let k = k as usize;
-        let mut contained = vec![false; 1 << k];
-        let (mut decimal, mut exp_cnt) = (0, 1 << k);
-        let mask = (1 << k) - 1;
-
-        for (i, &b) in s.as_bytes().iter().enumerate() {
-            decimal = ((decimal << 1) & mask) + if b == b'1' { 1 } else { 0 };
-            if i + 1 < k || contained[decimal] {
+        let _len_s: usize = s.len();
+        let ku: usize = k as usize;
+        let mut contained: Vec<bool> = vec![false; 1 << ku];
+        let mut mask: usize = 0;
+        let mut cnt_exp: u32 = 1 << k as u32;
+        let mask_all_ones: usize = (1 << ku) - 1;
+        for (idx, &b) in s.as_bytes().iter().enumerate(){
+            mask = ((mask << 1) & mask_all_ones) + if b == b'1' { 1 } else { 0 };
+            if idx + 1 < ku || contained[mask]{
                 continue;
             }
-            contained[decimal] = true;
-            exp_cnt -= 1;
-            if exp_cnt == 0 {
+            contained[mask] = true;
+            cnt_exp -= 1;
+            if cnt_exp == 0{
                 return true;
             }
         }
-
         false
     }
 }
