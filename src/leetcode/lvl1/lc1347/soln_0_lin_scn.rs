5681409f6607eb182d4a69c09aa339112a91d4d9
Leon <yanglyu.leon.7@gmail.com>
2022-06-02 23:43:25 -0700

    review

--- src/leetcode/lvl1/lc1347/soln_0_lin_scn.rs
+++ src/leetcode/lvl1/lc1347/soln_0_lin_scn.rs
@@ -10,20 +10,20 @@ impl Solution {
         let len_s: usize = s.len();
         let len_t: usize = t.len();
         // sanity check
-        if len_s != len_t{
+        if len_s != len_t {
             return -1;
         }
         let freqs_s: Vec<i32> = Self::get_hash(s);
         let freqs_t: Vec<i32> = Self::get_hash(t);
         let mut cnt_diff: i32 = 0;
-        for (idx, freq) in freqs_s.into_iter().enumerate(){
+        for (idx, freq) in freqs_s.into_iter().enumerate() {
             cnt_diff += (freq - freqs_t[idx]).abs();
         }
         cnt_diff / 2
     }
     fn get_hash(s: String) -> Vec<i32> {
         let mut freqs: Vec<i32> = vec![0; 26];
-        for ch in s.chars(){
+        for ch in s.chars() {
             freqs[ch as usize - 'a' as usize] += 1;
         }
         freqs
--- src/leetcode/lvl1/lc2186/soln_0_lin_scn.rs
+++ src/leetcode/lvl1/lc2186/soln_0_lin_scn.rs
@@ -1,30 +1,27 @@
 /// @author: Leon
 /// https://leetcode.com/problems/minimum-number-of-steps-to-make-two-strings-anagram-ii/
 /// Time Complexity:    O(max(`_len_s`, `_len_t`))
-/// Space Complexity:   O(1)
+/// Space Complexity:   O(max(`_len_s`, `_len_t`))
 struct Solution;
 
 #[allow(dead_code)]
 impl Solution {
     pub fn min_steps(s: String, t: String) -> i32 {
-        let freqs_s: Vec<u32> = {
-            let mut freqs: Vec<u32> = vec![0; 26];
-            for ch in s.chars() {
-                freqs[ch as usize - 'a' as usize] += 1;
-            }
-            freqs
-        };
-        let freqs_t: Vec<u32> = {
-            let mut freqs: Vec<u32> = vec![0; 26];
-            for ch in t.chars() {
-                freqs[ch as usize - 'a' as usize] += 1;
-            }
-            freqs
-        };
-        let mut cnt: i32 = 0;
-        for idx in 0..26 {
-            cnt += (freqs_s[idx] as i32 - freqs_t[idx] as i32).abs();
+        let _len_s: usize = s.len();
+        let _len_t: usize = t.len();
+        let hash_s: Vec<i32> = Self::get_hash(s);
+        let hash_t: Vec<i32> = Self::get_hash(t);
+        let mut cnt_diff: i32 = 0;
+        for (idx, freq) in hash_s.into_iter().enumerate() {
+            cnt_diff += (freq - hash_t[idx]).abs();
         }
-        cnt
+        cnt_diff
+    }
+    fn get_hash(s: String) -> Vec<i32> {
+        let mut freqs: Vec<i32> = vec![0; 26];
+        for ch in s.chars() {
+            freqs[ch as usize - 'a' as usize] += 1;
+        }
+        freqs
     }
 }
