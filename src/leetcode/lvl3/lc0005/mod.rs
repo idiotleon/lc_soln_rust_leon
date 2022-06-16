00f9ab16db1092bb51056c7f0117e80023723fa0
Leon <yanglyu.leon.7@gmail.com>
2022-06-16 16:44:39 -0700

    review

--- src/leetcode/lvl3/lc0005/mod.rs
+++ src/leetcode/lvl3/lc0005/mod.rs
@@ -1,1 +1,2 @@
-pub mod soln_0_dp_2d;
+pub mod soln_0_dp_2d_0;
+pub mod soln_0_dp_2d_1;
---
+++ src/leetcode/lvl3/lc0005/soln_0_dp_2d_1.rs
@@ -0,0 +1,14 @@
+/// @author: Leon
+/// https://leetcode.com/problems/longest-palindromic-substring/
+/// Time Complexity:    O()
+/// Space Complexity:   O()
+/// Reference:
+/// https://leetcode.com/problems/longest-palindromic-substring/discuss/2156691/Simple-DP-Solution-or-JAVA-Explained
+struct Solution;
+
+#[allow(dead_code)]
+impl Solution {
+    // pub fn longest_palindrome(s: String) -> String {
+
+    // }
+}
