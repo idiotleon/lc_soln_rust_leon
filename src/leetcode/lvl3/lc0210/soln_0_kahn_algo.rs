7043407fb3ab719388a6dd5a2facde35a3677af6
Leon <yanglyu.leon.7@gmail.com>
2022-06-22 23:02:02 -0700

    review

--- src/leetcode/lvl3/lc0210/soln_0_kahn_algo.rs
+++ src/leetcode/lvl3/lc0210/soln_0_kahn_algo.rs
@@ -1,52 +1,47 @@
+use std::collections::VecDeque;
 /// @author: Leon
 /// https://leetcode.com/problems/course-schedule-ii/
 /// Time Complexity:    O(`num_courses` + `_n_pres`)
 /// Space Complexity:   O(`num_courses` + `n_pres`)
-use std::collections::VecDeque;
-
-#[allow(dead_code)]
 struct Solution;
 
 #[allow(dead_code)]
 impl Solution {
-    pub fn find_order(num_courses: u16, prerequisites: Vec<Vec<u16>>) -> Vec<u16> {
+    pub fn find_order(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> Vec<i32> {
+        let len_cs: usize = num_courses as usize;
         let _n_pres = prerequisites.len();
-        let (graph, mut indegrees) = Self::build_graph(num_courses, prerequisites);
+        let (graph, mut indegrees) = Self::build_graph(len_cs, prerequisites);
         let mut queue: VecDeque<usize> = VecDeque::new();
         for (idx, &indegree) in indegrees.iter().enumerate() {
             if indegree == 0 {
                 queue.push_back(idx);
             }
         }
-        let mut idx: usize = 0;
-        let ans: Vec<u16> = {
-            let mut ans: Vec<u16> = vec![0; num_courses as usize];
-            while !queue.is_empty() {
-                let cur = queue.pop_front().unwrap();
-                ans[idx] = cur as u16;
-                idx += 1;
-                for &prev in graph[cur].iter() {
-                    indegrees[prev as usize] -= 1;
-                    if indegrees[prev as usize] == 0 {
-                        queue.push_back(prev as usize);
+        let mut ans: Vec<i32> = Vec::with_capacity(len_cs);
+        while !queue.is_empty() {
+            if let Some(cur) = queue.pop_front() {
+                ans.push(cur as i32);
+                for &nxt in graph[cur].iter() {
+                    indegrees[nxt] -= 1;
+                    if indegrees[nxt] == 0 {
+                        queue.push_back(nxt);
                     }
                 }
             }
-            ans
-        };
-        if idx < num_courses as usize {
+        }
+        if ans.len() < len_cs {
             return vec![];
         }
         ans
     }
-    fn build_graph(num_courses: u16, prerequisites: Vec<Vec<u16>>) -> (Vec<Vec<u16>>, Vec<u16>) {
-        let mut indegrees: Vec<u16> = vec![0; num_courses as usize];
-        let mut graph: Vec<Vec<u16>> = vec![vec![]; num_courses as usize];
+    fn build_graph(let_vts: usize, prerequisites: Vec<Vec<i32>>) -> (Vec<Vec<usize>>, Vec<u16>) {
+        let mut indegrees: Vec<u16> = vec![0; let_vts];
+        let mut graph: Vec<Vec<usize>> = vec![vec![]; let_vts];
         for pres in prerequisites {
-            let cur = pres[0];
-            let pre = pres[1];
-            graph[pre as usize].push(cur);
-            indegrees[cur as usize] += 1;
+            let cur: usize = pres[0] as usize;
+            let pre: usize = pres[1] as usize;
+            graph[pre].push(cur);
+            indegrees[cur] += 1;
         }
         (graph, indegrees)
     }
@@ -57,18 +52,18 @@ mod test {
     use super::*;
     #[test]
     fn test_sample_input_1() {
-        let num_courses: u16 = 2;
-        let prerequisites: Vec<Vec<u16>> = vec![vec![1, 0]];
+        let num_courses: i32 = 2;
+        let prerequisites: Vec<Vec<i32>> = vec![vec![1, 0]];
         let actual = Solution::find_order(num_courses, prerequisites);
-        let expected: Vec<Vec<u16>> = vec![vec![0, 1]];
-        assert!(true == expected.contains(&actual));
+        let expected: Vec<Vec<i32>> = vec![vec![0, 1]];
+        assert!(expected.contains(&actual));
     }
     #[test]
     fn test_sample_input_2() {
-        let num_courses: u16 = 4;
-        let prerequisites: Vec<Vec<u16>> = vec![vec![1, 0], vec![2, 0], vec![3, 1], vec![3, 2]];
-        let actual: Vec<u16> = Solution::find_order(num_courses, prerequisites);
-        let expected: Vec<Vec<u16>> = vec![vec![0, 2, 1, 3], vec![0, 1, 2, 3], vec![0, 2, 1, 3]];
-        assert!(true == expected.contains(&actual));
+        let num_courses: i32 = 4;
+        let prerequisites: Vec<Vec<i32>> = vec![vec![1, 0], vec![2, 0], vec![3, 1], vec![3, 2]];
+        let actual = Solution::find_order(num_courses, prerequisites);
+        let expected: Vec<Vec<i32>> = vec![vec![0, 2, 1, 3], vec![0, 1, 2, 3], vec![0, 2, 1, 3]];
+        assert!(expected.contains(&actual));
     }
 }
