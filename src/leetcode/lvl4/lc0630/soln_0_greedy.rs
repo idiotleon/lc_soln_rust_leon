6b7741b92fbfba8f863fe8a8fa9ced551e5efe94
Leon <yanglyu.leon.7@gmail.com>
2022-06-22 21:57:51 -0700

    review

--- src/leetcode/lvl4/lc0630/soln_0_greedy.rs
+++ src/leetcode/lvl4/lc0630/soln_0_greedy.rs
@@ -1,5 +1,4 @@
 use std::collections::BinaryHeap;
-use std::cmp::Reverse;
 /// @author: Leon
 /// https://leetcode.com/problems/course-schedule-iii/
 /// Time Complexity:    O(`len_n` * lg(`len_n`))
@@ -9,22 +8,23 @@ struct Solution;
 #[allow(dead_code)]
 impl Solution {
     pub fn schedule_course(courses: Vec<Vec<i32>>) -> i32 {
-        let len_cs: usize = courses.len();
+        let _len_cs: usize = courses.len();
         let sorted: Vec<Vec<i32>> = {
             let mut courses = courses;
             courses.sort_by_key(|e| e[1]);
             courses
         };
-        let mut heap: BinaryHeap<Reverse<i32>> = BinaryHeap::new();
+        // the max heap
+        let mut heap: BinaryHeap<i32> = BinaryHeap::new();
         // the running/overrall duration
         let mut sum: i32 = 0;
-        for course in sorted{
+        for course in sorted {
             let duration = course[0];
             let last_day = course[1];
-            heap.push(Reverse(duration));
+            heap.push(duration);
             sum += duration;
-            if sum > last_day{
-                if let Some(Reverse(top)) = heap.pop(){
+            if sum > last_day {
+                if let Some(top) = heap.pop() {
                     sum -= top;
                 }
             }
@@ -33,4 +33,14 @@ impl Solution {
     }
 }
 
-#[cfg()]
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    pub fn test_with_test_case_42_should_return_expected() {
+        let courses: Vec<Vec<i32>> = vec![vec![5, 5], vec![4, 6], vec![2, 6]];
+        let actual = Solution::schedule_course(courses);
+        let expected = 2;
+        assert_eq!(expected, actual);
+    }
+}
