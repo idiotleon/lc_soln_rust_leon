994cfd4e4291cf09bf8a1d3fb6e7952e767cc532
Leon <yanglyu.leon.7@gmail.com>
2022-06-29 12:02:21 -0700

    review

--- src/leetcode/lvl2/lc1564/soln_0_two_ptrs_0.rs
+++ src/leetcode/lvl2/lc1564/soln_0_two_ptrs_0.rs
@@ -1,9 +1,7 @@
-use std::cmp::min;
-
 /// @author: Leon
 /// https://leetcode.com/problems/put-boxes-into-the-warehouse-i/
-/// Time Complexity:    O()
-/// Space Complexity:   O()
+/// Time Complexity:    O(`_len_bs` * lg(`_len_bs`))
+/// Space Complexity:   O(lg(`_len_bs`))
 /// Reference:
 /// https://leetcode.com/problems/put-boxes-into-the-warehouse-i/discuss/821966/C%2B%2B-Two-Approaches
 struct Solution;
@@ -11,20 +9,21 @@ struct Solution;
 #[allow(dead_code)]
 impl Solution {
     pub fn max_boxes_in_warehouse(boxes: Vec<i32>, warehouse: Vec<i32>) -> i32 {
-        let len_wh = warehouse.len();
-        let len_b = boxes.len();
-        let mut count: usize = 0;
-        let mut boxes = boxes;
-        boxes.sort();
-        let mut warehouse = warehouse;
-        for i in 1..len_wh {
-            warehouse[i] = min(warehouse[i], warehouse[i - 1]);
-        }
-        for i in (0..len_wh).rev() {
-            if count < len_b && boxes[count] <= warehouse[i] {
-                count += 1;
+        let _len_bs: usize = boxes.len();
+        let len_ws: usize = warehouse.len();
+        let boxes: Vec<i32> = {
+            let mut boxes = boxes;
+            boxes.sort_by(|a, b| b.cmp(a));
+            boxes
+        };
+        let mut cnt: usize = 0;
+        for b in boxes {
+            cnt += if cnt < len_ws && b <= warehouse[cnt] {
+                1
+            } else {
+                0
             }
         }
-        count as i32
+        cnt as i32
     }
 }
--- src/leetcode/lvl2/lc1564/soln_0_two_ptrs_1.rs
+++ src/leetcode/lvl2/lc1564/soln_0_two_ptrs_1.rs
@@ -1,7 +1,7 @@
 /// @author: Leon
 /// https://leetcode.com/problems/put-boxes-into-the-warehouse-i/
-/// Time Complexity:    O()
-/// Space Complexity:   O()
+/// Time Complexity:    O(max(`len_bs`, `len_ws`) * lg(max(`len_bs`, `len_ws`)))
+/// Space Complexity:   O(lg(max(`len_bs`, `len_ws`)))
 /// Reference:
 /// https://leetcode.com/problems/put-boxes-into-the-warehouse-i/discuss/821966/C%2B%2B-Two-Approaches
 struct Solution;
@@ -9,15 +9,28 @@ struct Solution;
 #[allow(dead_code)]
 impl Solution {
     pub fn max_boxes_in_warehouse(boxes: Vec<i32>, warehouse: Vec<i32>) -> i32 {
-        let len_wh = warehouse.len();
-        let mut count: usize = 0;
-        let mut boxes = boxes;
-        boxes.sort_by(|a, b| b.partial_cmp(a).unwrap());
-        for &b in boxes.iter() {
-            if count < len_wh && b <= warehouse[count] {
-                count += 1;
+        let len_bs: usize = boxes.len();
+        let len_ws: usize = warehouse.len();
+        let boxes: Vec<i32> = {
+            let mut boxes = boxes;
+            boxes.sort();
+            boxes
+        };
+        let wh: Vec<i32> = {
+            let mut wh = warehouse;
+            for idx in 1..len_ws {
+                wh[idx] = std::cmp::min(wh[idx - 1], wh[idx]);
+            }
+            wh
+        };
+        let mut cnt: usize = 0;
+        for idx in (0..len_ws).rev() {
+            cnt += if cnt < len_bs && boxes[cnt] <= wh[idx] {
+                1
+            } else {
+                0
             }
         }
-        count as i32
+        cnt as i32
     }
 }
