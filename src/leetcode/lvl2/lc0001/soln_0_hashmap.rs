e0b78cfa563aa9aaa0802fbbe73406ba90d735f8
Leon <yanglyu.leon.7@gmail.com>
2022-07-19 00:36:52 -0700

    review

--- src/leetcode/lvl2/lc0001/soln_0_hashmap.rs
+++ src/leetcode/lvl2/lc0001/soln_0_hashmap.rs
@@ -2,9 +2,8 @@ use std::collections::HashMap;
 
 /// @author: Leon
 /// https://leetcode.com/problems/two-sum/
-/// Time Complexity:    O(`_len_n`)
-/// Space Complexity:   O(`_len_n`)
-/// this version consumes the original vector
+/// Time Complexity:    O(`len_ns`)
+/// Space Complexity:   O(`len_ns`)
 /// Reference:
 /// https://leetcode.com/problems/two-sum/discuss/715951/Rust%3A-HashMap-solution
 struct Solution;
@@ -12,13 +11,15 @@ struct Solution;
 #[allow(dead_code)]
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let _len_n = nums.len();
-        let mut num_to_idx = HashMap::<i32, usize>::new();
+        let len_ns = nums.len();
+        let mut num_to_idx: HashMap<i32, usize> = HashMap::with_capacity(len_ns);
         for (idx, num) in nums.into_iter().enumerate() {
             let expected_sum = target - num;
             match num_to_idx.get(&expected_sum) {
-                Some(&prev_idx) => return vec![prev_idx as i32, idx as i32],
-                _ => {
+                Some(&prev_idx) => {
+                    return vec![prev_idx as i32, idx as i32];
+                }
+                None => {
                     num_to_idx.insert(num, idx);
                 }
             }
