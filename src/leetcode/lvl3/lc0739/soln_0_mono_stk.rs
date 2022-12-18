c8148d7ae2ce7067b5af12ea4d111a9f7088df71
Leon <yanglyu.leon.7@gmail.com>
2022-12-17 19:52:00 -0800

    review

--- src/leetcode/lvl3/lc0739/soln_0_mono_stk.rs
+++ src/leetcode/lvl3/lc0739/soln_0_mono_stk.rs
@@ -1,34 +1,31 @@
+use std::collections::VecDeque;
+
 /// @author: Leon
 /// https://leetcode.com/problems/daily-temperatures/
 /// Time Complexity:    O(`len_t`)
 /// Space Complexity:   O(`len_t`)
 struct Solution;
 
-use std::collections::VecDeque;
-
 #[allow(dead_code)]
 impl Solution {
-    pub fn daily_temperatures(temperatures: Vec<i32>) -> Vec<i32> {
-        let len_t = temperatures.len();
-        let mut stk: VecDeque<usize> = VecDeque::new();
-        let ans: Vec<i32> = {
-            let mut tmp = vec![0; len_t];
-            for (idx, &t) in temperatures.iter().enumerate() {
-                while let Some(&idx_prev) = stk.back() {
-                    if temperatures[idx_prev] < t {
-                        tmp[idx_prev] = (idx - idx_prev) as i32;
-                        stk.pop_back();
-                    } else {
-                        break;
-                    }
+    pub fn daily_temperatures(ts: Vec<i32>) -> Vec<i32> {
+        let len_ts: usize = ts.len();
+        let mut stk: VecDeque<usize> = VecDeque::with_capacity(len_ts);
+        let mut ans: Vec<i32> = vec![0; len_ts];
+        for (idx, &num) in ts.iter().enumerate() {
+            while let Some(&idx_prev) = stk.back() {
+                if ts[idx_prev] < num {
+                    ans[idx_prev] = (idx - idx_prev) as i32;
+                    stk.pop_back();
+                } else {
+                    break;
                 }
-                stk.push_back(idx);
             }
-            while let Some(idx) = stk.pop_back() {
-                tmp[idx] = 0;
-            }
-            tmp
-        };
-        ans
+            stk.push_back(idx);
+        }
+        while let Some(idx) = stk.pop_back() {
+            ans[idx] = 0;
+        }
+        return ans;
     }
 }
