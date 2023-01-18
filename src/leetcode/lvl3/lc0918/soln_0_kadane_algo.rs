0ded10ac0b7126563d00891e5f4d5a1a6f800b05
Leon <yanglyu.leon.7@gmail.com>
2023-01-17 23:05:05 -0800

    review

--- src/leetcode/lvl3/lc0918/soln_0_kadane_algo.rs
+++ src/leetcode/lvl3/lc0918/soln_0_kadane_algo.rs
@@ -1,5 +1,3 @@
-use std::cmp::{max, min};
-
 /// @author: Leon
 /// https://leetcode.com/problems/maximum-sum-circular-subarray/
 /// Time Complexity:    O(`_len_n`)
@@ -11,23 +9,23 @@ struct Solution;
 #[allow(dead_code)]
 impl Solution {
     pub fn max_subarray_sum_circular(nums: Vec<i32>) -> i32 {
-        let _len_n: usize = nums.len();
+        let _len_ns: usize = nums.len();
         let mut sum_total: i32 = 0;
         let mut max_global: i32 = nums[0];
         let mut max_local: i32 = 0;
         let mut min_global: i32 = nums[0];
         let mut min_local: i32 = 0;
         for num in nums {
-            max_local = max(max_local + num, num);
-            max_global = max(max_global, max_local);
-            min_local = min(min_local + num, num);
-            min_global = min(min_global, min_local);
+            max_local = std::cmp::max(max_local + num, num);
+            max_global = std::cmp::max(max_global, max_local);
+            min_local = std::cmp::min(min_local + num, num);
+            min_global = std::cmp::min(min_global, min_local);
             sum_total += num;
         }
-        if max_global > 0 {
-            max(max_global, sum_total - min_global)
+        return if max_global > 0 {
+            std::cmp::max(max_global, sum_total - min_global)
         } else {
             max_global
-        }
+        };
     }
 }
