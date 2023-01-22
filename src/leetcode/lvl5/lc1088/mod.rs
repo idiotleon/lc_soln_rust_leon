46e8a5ef2d5de9785434fc98f333896e499a6ead
Leon <yanglyu.leon.7@gmail.com>
2023-01-22 13:44:46 -0800

    review

--- src/leetcode/lvl5/lc1088/mod.rs
+++ src/leetcode/lvl5/lc1088/mod.rs
@@ -1,1 +1,1 @@
-pub mod soln_0_dfs_r;
+pub mod soln_0_backtrack_r;
--- src/leetcode/lvl5/lc1088/soln_0_backtrack_r.rs
+++ src/leetcode/lvl5/lc1088/soln_0_backtrack_r.rs
@@ -2,8 +2,8 @@ use std::collections::HashMap;
 
 /// @author: Leon
 /// https://leetcode.com/problems/confusing-number-ii/
-/// Time Complexity:    O()
-/// Space Complexity:   O()
+/// Time Complexity:    O(len_of_n * 6) ~ O(len_of_n)
+/// Space Complexity:   O(6) ~ O(1)
 /// Reference:
 /// https://leetcode.com/problems/confusing-number-ii/discuss/446589/Easy-to-understand-Java-backtracking-solution-covers-edge-case
 struct Solution;
@@ -13,7 +13,7 @@ impl Solution {
     pub fn confusing_number_ii(n: i32) -> i32 {
         // candidates to their rotated equivalents
         let cand_to_rot: HashMap<i64, i64> = {
-            let mut map: HashMap<i64, i64> = HashMap::with_capacity(5);
+            let mut map: HashMap<i64, i64> = HashMap::with_capacity(6);
             map.insert(0, 0);
             map.insert(1, 1);
             map.insert(6, 9);
@@ -22,25 +22,25 @@ impl Solution {
             map
         };
         let mut cnt: i32 = 0;
-        Self::dfs(0, n as i64, &cand_to_rot, &mut cnt);
+        Self::backtrack(0, n as i64, &cand_to_rot, &mut cnt);
         return cnt;
     }
-    fn dfs(num_cur: i64, n: i64, cand_to_rot: &HashMap<i64, i64>, cnt: &mut i32) {
+    fn backtrack(num_cur: i64, n: i64, cand_to_rot: &HashMap<i64, i64>, cnt: &mut i32) {
         if Self::is_confusing(num_cur, cand_to_rot) {
             *cnt += 1;
         }
         for (&candidate, &_rotated) in cand_to_rot.iter() {
             let num_nxt = num_cur * 10 + candidate;
             if num_nxt <= n && num_nxt != 0 {
-                Self::dfs(num_nxt, n, cand_to_rot, cnt);
+                Self::backtrack(num_nxt, n, cand_to_rot, cnt);
             }
         }
     }
     fn is_confusing(n: i64, cand_to_rot: &HashMap<i64, i64>) -> bool {
         let mut num = n;
-        let mut res = 0;
+        let mut rotated = 0;
         while num > 0 {
-            res = res * 10
+            rotated = rotated * 10
                 + if let Some(&cand) = cand_to_rot.get(&(num % 10)) {
                     cand
                 } else {
@@ -48,6 +48,7 @@ impl Solution {
                 };
             num /= 10;
         }
-        return res as i64 != n;
+        // to make sure it is NOT the original `num`
+        return rotated as i64 != n;
     }
 }
