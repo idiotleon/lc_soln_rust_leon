a62e7c08609cb6617102b2ce460b637f74e70b2c
Leon <yanglyu.leon.7@gmail.com>
2023-04-09 11:34:50 -0700

    review

--- src/leetcode/lvl4/lc1857/soln_0_topo_srt.rs
+++ src/leetcode/lvl4/lc1857/soln_0_topo_srt.rs
@@ -12,13 +12,12 @@ struct Solution;
 #[allow(dead_code)]
 impl Solution {
     pub fn largest_path_value(colors: String, edges: Vec<Vec<i32>>) -> i32 {
-        let len_c: usize = colors.len();
-        // not used
-        // let len_e: usize = edges.len();
+        let len_cs: usize = colors.len();
+        let _len_es: usize = edges.len();
         let chs: Vec<char> = colors.chars().collect();
         let (graph, mut indegrees) = {
             let mut graph = HashMap::<u32, HashSet<u32>>::new();
-            let mut indegrees: Vec<u32> = vec![0; len_c];
+            let mut indegrees: Vec<u32> = vec![0; len_cs];
             for edge in edges.into_iter() {
                 let u: u32 = edge[0] as u32;
                 let v: u32 = edge[1] as u32;
@@ -28,7 +27,7 @@ impl Solution {
             (graph, indegrees)
         };
         let mut queue = VecDeque::<u32>::new();
-        let mut ch_to_chs_freqs: Vec<Vec<u32>> = vec![vec![0; 26]; len_c];
+        let mut ch_to_chs_freqs: Vec<Vec<u32>> = vec![vec![0; 26]; len_cs];
         for (idx, &indegree) in indegrees.iter().enumerate() {
             if indegree == 0 {
                 queue.push_back(idx as u32);
@@ -37,38 +36,33 @@ impl Solution {
         }
         let mut seen: usize = 0;
         let mut largest: i32 = 0;
-        while !queue.is_empty() {
-            if let Some(cur) = queue.pop_front() {
-                seen += 1;
-                if let Some(&cur_max) = ch_to_chs_freqs[cur as usize].iter().max() {
-                    largest = max(largest, cur_max as i32);
-                }
-                if let Some(next_vertices) = graph.get(&(cur as u32)) {
-                    for &next in next_vertices.iter() {
-                        let next: usize = next as usize;
-                        for i in 0..26 {
-                            ch_to_chs_freqs[next][i] = max(
-                                ch_to_chs_freqs[next][i],
-                                ch_to_chs_freqs[cur as usize][i]
-                                    + if chs[next] as usize - 'a' as usize == i {
-                                        1
-                                    } else {
-                                        0
-                                    },
-                            );
-                        }
-                        indegrees[next] -= 1;
-                        if indegrees[next] == 0 {
-                            queue.push_back(next as u32);
-                        }
+        while let Some(cur) = queue.pop_front() {
+            seen += 1;
+            if let Some(&cur_max) = ch_to_chs_freqs[cur as usize].iter().max() {
+                largest = max(largest, cur_max as i32);
+            }
+            if let Some(next_vertices) = graph.get(&(cur as u32)) {
+                for &next in next_vertices.iter() {
+                    let next: usize = next as usize;
+                    for i in 0..26 {
+                        ch_to_chs_freqs[next][i] = max(
+                            ch_to_chs_freqs[next][i],
+                            ch_to_chs_freqs[cur as usize][i]
+                                + if chs[next] as usize - 'a' as usize == i {
+                                    1
+                                } else {
+                                    0
+                                },
+                        );
+                    }
+
+                    indegrees[next] -= 1;
+                    if indegrees[next] == 0 {
+                        queue.push_back(next as u32);
                     }
                 }
             }
         }
-        if seen == len_c {
-            largest
-        } else {
-            -1
-        }
+        return if seen == len_cs { largest } else { -1 };
     }
 }
