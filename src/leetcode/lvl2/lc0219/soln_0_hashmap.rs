cbf8ffad169b59d54d44316942b6ecef20effc00
Leon <yanglyu.leon.7@gmail.com>
2024-02-25 08:52:38 -0800

    review

--- src/leetcode/lvl2/lc0219/soln_0_hashmap.rs
+++ src/leetcode/lvl2/lc0219/soln_0_hashmap.rs
@@ -2,16 +2,16 @@ use std::collections::HashMap;
 
 /// @author: Leon
 /// https://leetcode.com/problems/contains-duplicate-ii/
-/// Time Complexity:    O(`_len_n`)
-/// Space Complexity:   O(`_len_n`)
+/// Time Complexity:    O(`len_ns`)
+/// Space Complexity:   O(`len_ns`)
 #[allow(dead_code)]
 struct Solution;
 
 #[allow(dead_code)]
 impl Solution {
     pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
-        let _len_n = nums.len();
-        let mut num_to_idx: HashMap<i32, usize> = HashMap::new();
+        let len_ns = nums.len();
+        let mut num_to_idx: HashMap<i32, usize> = HashMap::with_capacity(len_ns);
         for (idx, num) in nums.into_iter().enumerate() {
             if let Some(&prev_idx) = num_to_idx.get(&num) {
                 if idx - prev_idx <= k as usize {
--- src/leetcode/lvl2/lc0219/soln_0_hashset.rs
+++ src/leetcode/lvl2/lc0219/soln_0_hashset.rs
@@ -2,8 +2,8 @@ use std::collections::HashSet;
 
 /// @author: Leon
 /// https://leetcode.com/problems/contains-duplicate-ii/
-/// Time Complexity:    O(`_len_n`)
-/// Space Complexity:   O(`_len_n`)
+/// Time Complexity:    O(`len_ns`)
+/// Space Complexity:   O(`len_ns`)
 /// Reference:
 /// https://leetcode.com/problems/contains-duplicate-ii/discuss/61599/C%2B%2B-unordered_map-and-unordered_set
 #[allow(dead_code)]
@@ -12,9 +12,9 @@ struct Solution;
 #[allow(dead_code)]
 impl Solution {
     pub fn contains_nearby_duplicate(nums: Vec<i32>, k: i32) -> bool {
-        let _len_n: usize = nums.len();
+        let len_ns: usize = nums.len();
         let k: usize = k as usize;
-        let mut set: HashSet<i32> = HashSet::new();
+        let mut set: HashSet<i32> = HashSet::with_capacity(len_ns);
         for (idx, &num) in nums.iter().enumerate() {
             if idx > k as usize {
                 set.remove(&nums[idx - k - 1]);
